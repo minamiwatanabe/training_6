4367a42ab5b0ac335e2e2315559d220a39338d7f
minamiwatanabe <himawari.bamu@gmial.com>
2025-04-07 16:40:06 +0900

    ファイル追加

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>summit_LP</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+  <img src="./images/stocksun_logo.png" alt="">
+  
