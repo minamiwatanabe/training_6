ba996849147b71d593fcb007f053dbf83c6c42d1
minamiwatanabe <himawari.bamu@gmial.com>
2025-04-11 05:08:27 +0900

    変更

--- header.php
+++ header.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>summit_LP</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-  <img src="./images/stocksun_logo.png" alt="">
+
   
--- index.php
+++ index.php
@@ -1,4 +1,36 @@
 <?php include('header.php'); ?>
 
+<div class="kv">
+    <div class="kv_container">
+        <img class="stocksun_logo" src="./images/stocksun_logo.png" alt="stocksunロゴ">
+            <div class="kv_center">
+                <p class="subtitle">日本最大規模のフリーランスイベント</p>
+                <picture>
+                    <source srcset="./images/sp/logo.png" media="(max-width: 767px)">
+                    <img class="summit_logo" src="./images/kv.png" alt="summitロゴ">
+                </picture>
+                <img class="summit_sub_logo" src="./images/sp/sub_title.png" alt="summitサブロゴ">                  
+                <div class="time_logo_box">
+                    <img class="time_logo" src="./images/日付.png" alt="日付">
+                </div>
+                <p class="location">東京国際フォーラム  ホールE2</p>
+                <img class="line_button" src="./images/button.png" alt="LINE">
+            </div>
+    </div>
+</div>
+<div class="fixed_button">
+    <a href="#">
+        <img src="./images/side_button.png" alt="お問い合わせ" class="side_button_image">
+        <img src="./images/sp/button_sp.png" alt="お問い合わせ" class="side_button_sp_image">
+    </a>
+</div>
+<div class="section">
+    <div class="section_1_2">
+        <div class="section_1">
+            <p class="sectiontitle">Freelance Summit</p>
+            <p class="sectionsub_title">フリーランスサミットとは？</p>
+        </div>
+    </div>
+</div>
 
 <?php include('footer.php'); ?>
