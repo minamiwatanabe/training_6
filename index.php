d0e7a5492e0210aabf8659782db1dd88e9df2f3a
minamiwatanabe <himawari.bamu@gmial.com>
2025-04-17 15:07:00 +0900

    変更

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
                 </picture>
                 <div class="summit_sub_logo">
                     <p class="summit_sub_text">sponsored by</p>
-                    <p class="summit_sub_text2">DMYテック</p>  
+                    <img class="DMY_logo" src="./images/sp/DYM.png" alt="DMYテック">
                 </div>             
                 <div class="time_logo_box">
-                    <p class="time_logo">3/4 <span class="sat">sat</span> 10:00-18:00</p>
+                    <p class="time_logo">3/4 <span class="sat">Sat</span> 10:00-18:00</p>
                 </div>
                 <p class="location">東京国際フォーラム  ホールE2</p>
                 <img class="line_button" src="./images/button.png" alt="LINE">
@@ -38,6 +38,181 @@
             </div>
             <p class="section_1_description">各業界でTOPクラスのフリーランスが集結するイベント。有名企業が集結する40ブースもの出展エリアに加え、各領域で圧倒的に結果を出し続ける精鋭フリーランス達が、自身のノウハウを語ります。他のイベントでは味わえない、新たな体験と出会える充実したコンテンツを用意しています。</p>
             <p class="sp_section1_text">各業界でTOPクラスのフリーランスが集結するイベント。有名企業が集結する40ブースもの出展エリアに加え、各領域で圧倒的に結果を出し続ける精鋭フリーランス達が、自身のノウハウを語ります。他のイベントでは味わえない、新たな体験と出会える充実したコンテンツを用意しています。</p>
+        </div>
+        <div class="section_2">
+            <h1 class="sectiontitle">Event Summary</h1>
+            <h2 class="sectionsub_title">イベント開催概要</h2>
+            <div class="info_block">
+                <div class="info_item">
+                    <div class="info_label">開催日程</div>
+                    <p class="info_content">2023年3月4日（土）開場10:00 閉場18:00（受付開始9:30-）</p>
+                    <p class="sp_info_content">2023年3月4日（土）10:00〜18:00 <br>（受付開始9:30-）</p>
+                </div>
+                <div class="info_item">
+                    <div class="info_label">会場</div>
+                    <p class="info_content">東京国際フォーラム ホールE2</p>
+                    <p class="sp_info_content">東京国際ホール</p>
+                </div>
+                <div class="info_item">
+                    <div class="info_label">イベント内容</div>
+                    <p class="info_content">企業ブース出展：60ブース <br> ビジネス系セミナー：12枠</p>
+                    <p class="sp_info_content">企業ブース出展：60ブース <br> ビジネス系セミナー：12枠</p>
+                </div>
+                <div class="info_item">
+                    <div class="info_label">対象者</div>
+                    <p class="info_content">経営者 / フリーランス / 独立志望の会社員</p>
+                    <p class="sp_info_content">経営者 / フリーランス / <br>独立志望の会社員</p>
+                </div>
+                <div class="info_item">
+                    <div class="info_label">アクセス</div>
+                    <div class="info_content">
+                        <div class="access_text">
+                            <p class="access_title">【最寄駅】</p>
+                            <p class="access">
+                                JR<br>
+                                <span class="line">有楽町駅より徒歩1分</span><br>
+                                <span class="line">東京駅より徒歩5分</span>
+                            </p>
+                            <p class="access">
+                                地下鉄<br>
+                                <span class="line">有楽町線 ： 有楽町駅(B1F地下コンコースにて連絡)</span><br>
+                                <span class="line">千代田線 ： 二重橋前駅より徒歩5分/日比谷駅より徒歩7分</span><br>
+                                <span class="line">丸ノ内線 ： 銀座駅より徒歩5分</span><br>
+                                <span class="line">銀座線 ： 銀座駅より徒歩7分/京橋駅より徒歩7分</span><br>
+                                <span class="line">三田線 ： 日比谷駅より徒歩5分</span>
+                            </p>
+                        </div>
+                        <div class="map_container">
+                            <img class="map_image"src="./images/map.png" alt="東京国際ホール">
+                        </div>
+                    </div>
+                </div>
+                <div class="sp_map_container">
+                    <img class="map_image"src="./images/sp/sp_map.png" alt="東京国際ホール">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section_3_contener">
+        <div class="section_3">
+            <h1 class="sectiontitle">Contents</h1>
+            <h2 class="sectionsub_title">イベント内容</h2>
+            <div class="section_3_main">
+                <div class="main_conteiner">
+                    <h1 class="main_title">フリーランスに特化した企業エリア</h1>
+                    <div class="main_box">
+                        <p class="main_text1">出会いたい企業にきっと出会える</p>
+                        <img src="./images/Vector 15.png" alt="" class="sec3_line">
+                        <h2 class="main_text2"><span class="quantity">60</span>ブースの企業エリア</h2>
+                        <img src="./images/area_img.png" alt="ブースイメージ" class="sec3_img">
+                    </div>
+                </div>
+                <div class="main_conteiner">
+                <h1 class="main_title">各業界の著名人が登壇</h1>
+                    <div class="main_box">
+                        <p class="main_text1">ここでしか聞けない話が聞ける</p>
+                        <img src="./images/Vector 15.png" alt="" class="sec3_line">
+                        <h2 class="main_text2">豪華セミナーを開催</h2>
+                        <img src="./images/seminar_img.png" alt="セミナーイメージ" class="sec3_img">
+                    </div>
+                </div>
+            </div>
+            <img class="line_button" src="./images/button.png" alt="LINE">
+        </div>
+    </div>
+    <div class="section_4_contener">
+        <div class="section_4">
+            <h1 class="sectiontitle">Pick Up</h1>
+            <h2 class="sectionsub_title">注目の企業</h2>
+            <div class="section_4_main">
+                <div class="company_box">
+                    <div class="sec4_img_box">
+                        <img src="./images/company/company1.png" alt="オープラン株式会社" class="company1_img">
+                    </div>
+                    <div class="company_about_box">
+                        <p class="company_about">オープラン株式会社は「ITサービス」を軸に「人」と「グローバル」に向き合い日本を代表する企業になるべく2015年に産声をあげたベンチャー企業です。</p>
+                        <a class="oplan_link" href="https://oplan.co.jp/">https://oplan.co.jp/</a>
+                    </div>
+                </div>
+                <div class="company_box">
+                    <div class="sec4_img_box">
+                        <img src="./images/company/company2.png" alt="PE-BANK" class="company2_img">
+                    </div>
+                    <div class="company_about_box">
+                        <p class="company_about">ITフリーランスの仕事探しなら【PE-BANK】へ。Web・SE系を中心に毎月1,000件以上のご紹介。</p>
+                    </div>
+                </div>
+                <div class="company_box">
+                    <div class="sec4_img_box">
+                        <img src="./images/company/company3.png" alt="BOCジャパン合同株式会社" class="company3_img">
+                    </div>
+                    <div class="company_about_box">
+                        <p class="company_about">台湾の「食文化」の導入により、日本の飲食業をクリエイティブに。</p>
+                    </div>
+                </div>
+                <div class="company_box">
+                    <div class="sec4_img_box">
+                        <img src="./images/company/company4.png" alt="creava" class="company4_img">
+                    </div>
+                    <div class="company_about_box">
+                        <p class="company_about">月額制システム開発チームレンタルSucSak(サクサク)を運営している東京新宿のシステム開発会社です。</p>
+                        <div class="link_box">
+                            <img src="./images/company/company4a.png" alt="SucSak" class="company4a_img">
+                            <p class="colon">:</p>
+                            <a class="sucsak_link" href="https://sucsak.com/">https://sucsak.com/</a>
+                        </div>
+                    </div>    
+                </div>
+                <div class="company_box">
+                    <div class="sec4_img_box">
+                        <img src="./images/company/company5.png" alt="QUARTET" class="company5_img">
+                    </div>
+                    <div class="company_about_box">
+                        <p class="company_about">1000業種以上の圧倒的運用実績！リスティング、Facebook・TwitterなどのSNS広告もおまかせ。</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section_5_contener">
+        <div class="section_5">
+            <h1 class="sectiontitle">About Exhibitors</h1>
+            <h2 class="sectionsub_title">出展企業一覧</h2>
+            <div class="company_list">
+                <div class="col col_left">
+                    <p>オープラン株式会社</p>
+                    <p>株式会社ビズリンク</p>
+                    <p>株式会社カルテットコミュニケーションズ</p>
+                    <p>クリーヴァ株式会社</p>
+                    <p>株式会社グラントホープ</p>
+                    <p>Modis株式会社</p>
+                    <p>INTLOOP株式会社</p>
+                    <p>Langdemy株式会社</p>
+                    <p>BOCジャパン合同会社</p>
+                </div>
+                <div class="col  col_right">
+                    <p>株式会社アユザック</p>
+                    <p>株式会社HandAid</p>
+                    <p>株式会社J-Proach</p>
+                    <p>ラーメン協会</p>
+                    <p>株式会社おウチdeお肉</p>
+                    <p>株式会社LifeCoach</p>
+                    <p>株式会社スポーツ＆ライフイノベーション</p>
+                    <p>株式会社iGO</p>
+                    <p>株式会社エドモンド</p>
+                </div>
+            </div>
+            <p class="note">（順不同、一部企業のみ掲載）</p>
+            <div class="box_container">
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+                <div class="box"></div>
+            </div>
         </div>
     </div>
 </div>
